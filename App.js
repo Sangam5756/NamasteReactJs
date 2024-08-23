d6560035a25e8eb934a3f156722be853cca9f3cc
sangam5756 <sangammunde3@gmail.com>
2024-08-23 23:01:32 +0530

    multipleparent

--- App.js
+++ App.js
@@ -10,8 +10,22 @@ const parent = React.createElement("div", { id: "parent" }, [
   ]),
 ]);
 
+const parent1 = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Hiii h1 Broooo"),
+    React.createElement("h2", {}, "Hiii h2 Broooo"),
+  ]),
+
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Hiii h1 Broooo"),
+    React.createElement("h2", {}, "Hiii h2 Broooo"),
+  ]),
+]);
+
+const data = [parent, parent1];
+
 // Because of this complex structure there is something make our life easy
 // that is called the jsx
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(data);

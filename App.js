9b9b1cea0d1a9819769fbeff6f987d04576ec6c2
sangam5756 <sangammunde3@gmail.com>
2024-08-23 22:52:10 +0530

    Multiplechildren

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
+const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "Hiii h1 Broooo"),
     React.createElement("h2", {}, "Hiii h2 Broooo"),
-  ])
-);
+  ]),
+
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Hiii h1 Broooo"),
+    React.createElement("h2", {}, "Hiii h2 Broooo"),
+  ]),
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);

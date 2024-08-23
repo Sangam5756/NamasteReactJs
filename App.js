7512cb644e1290b09e927f2d370f4b994337f4d3
sangam5756 <sangammunde3@gmail.com>
2024-08-23 22:49:45 +0530

    multipleSiblings

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "Hiii h1 Broooo")
-  )
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Hiii h1 Broooo"),
+    React.createElement("h2", {}, "Hiii h2 Broooo"),
+  ])
 );
 
-
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);

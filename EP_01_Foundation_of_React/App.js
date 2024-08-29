9e3d9894f741544a5158e2e104134470b25cc1d7
sangam5756 <sangammunde3@gmail.com>
2024-08-29 20:34:17 +0530

    Foundation OF React

---
+++ EP_01_Foundation_of_React/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// Wrap your elements in a single parent element
+const jsxheading = (
+  <>
+    <a href="dasdsa">hiii</a>
+    <a href="dasdsa">hiii</a>
+  </>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxheading);

1ddfdf22c4c3647b0f5bab58d373c98fabc86123
sangam5756 <sangammunde3@gmail.com>
2024-09-05 22:14:27 +0530

    multiple way to import component

--- EP_01_Foundation_of_React/App.js
+++ EP_01_Foundation_of_React/App.js
@@ -5,29 +5,27 @@ import ReactDOM from "react-dom/client";
 
 const elem = <span>This is element</span>;
 
-// composing two components into one called component composition
-const HeadingComponent2 = () => (
-  <div>
-    {/* <Title/> */}
-    {title}
-    <h1>This is component 2</h1>
-  </div>
-);
- //react element  //gone to infinite loop
- const title = (
+
+//react element  //gone to infinite loop
+const Title =()=> (
   <h1>
-    Sangam {elem} <HeadingComponent2 /> Mundhe
+    Sangam {elem}Mundhe
   </h1>
 );
 
-const Title = () => <h1>Sangam Mundhe</h1>; //react element
-
-//react functional component
-// const HeadingComponent = () => {
-//   return <h1>This Is Sangam</h1>;
-// };
 
 
+
+// composing two components into one called component composition
+const HeadingComponent2 = () => (
+  <div>
+    {Title()}
+    <Title/>
+    <Title></Title>
+    <h1>This is component 2</h1>
+
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 

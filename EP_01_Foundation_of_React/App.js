db702871eff7d62999f7ba517abb0a64ac7fd53e
sangam5756 <sangammunde3@gmail.com>
2024-09-05 21:43:49 +0530

    FunctionalComponent

--- EP_01_Foundation_of_React/App.js
+++ EP_01_Foundation_of_React/App.js
@@ -2,13 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // Wrap your elements in a single parent element
-const jsxheading = (
-  <>
-    <a href="dasdsa">hiii</a>
-    <a href="dasdsa">hiii</a>
-  </>
+
+const Title =()=> <h1>Sangam Mundhe</h1>; //react element
+
+// const HeadingComponent = () => {
+//   //react functional component
+//   return <h1>This Is Sangam</h1>;
+// };
+
+
+const HeadingComponent2 = () =>(
+  <div>
+      <Title/>
+    <h1>This is component 2</h1>
+  </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxheading);
+root.render(<HeadingComponent2 />);

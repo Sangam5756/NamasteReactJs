3bbb74471e5297c31cbce7a870cbc6f9249b48be
sangam5756 <sangammunde3@gmail.com>
2024-09-07 12:37:38 +0530

    how to rendering works and props

---
+++ EP_02_Implementaion/data.js
@@ -0,0 +1,457 @@
+export const data = [
+    {    
+            id: "522738",
+            name: "Home Plate by EatFit",
+            cloudinaryImageId: "e975b355e30661b40e3522641689cede",
+            locality: "Kalyani Nagar Road",
+            areaName: "Wadgaon Sheri",
+            costForTwo: "₹250 for two",
+            cuisines: [
+                "Pizzas",
+                "Pastas",
+                "Burgers",
+                "Chinese",
+                "Italian",
+                "Desserts",
+                "Beverages",
+            ],
+            avgRating: 4.2,
+            parentId: "504312",
+            avgRatingString: "4.2",
+            totalRatingsString: "1.8K+",
+            sla: {
+                deliveryTime: 26,
+                lastMileTravel: 2.8,
+                serviceability: "SERVICEABLE",
+                slaString: "25-30 mins",
+                lastMileTravelString: "2.8 km",
+                iconType: "ICON_TYPE_EMPTY",
+            },
+            availability: {
+                nextCloseTime: "2024-09-07 23:45:00",
+                opened: true,
+            },
+            badges: {
+                textExtendedBadges: [
+                    {
+                        iconId: "guiltfree/GF_Logo_android_3x",
+                        shortDescription: "options available",
+                        fontColor: "#7E808C",
+                    },
+                ],
+            },
+            
+    },
+    
+    {
+       
+            id: "261214",
+            name: "Chinese Chef Co",
+            cloudinaryImageId: "wygwoaorvb3pctzetdsq",
+            locality: "Balaji Udhayam Nagar",
+            areaName: "Wadgaon Sheri",
+            costForTwo: "₹300 for two",
+            cuisines: ["Chinese", "Malaysian"],
+            avgRating: 3.7,
+            parentId: "61635",
+            avgRatingString: "3.7",
+            totalRatingsString: "6.4K+",
+            sla: {
+                deliveryTime: 30,
+                lastMileTravel: 2.6,
+                serviceability: "SERVICEABLE",
+                slaString: "25-30 mins",
+                lastMileTravelString: "2.6 km",
+                iconType: "ICON_TYPE_EMPTY",
+            },
+            availability: {
+                nextCloseTime: "2024-09-08 06:00:00",
+                opened: true,
+            },
+          
+       
+       
+    },
+    {
+   
+            id: "261215",
+            name: "Chinese Culture",
+            cloudinaryImageId: "dsg7ozmt4swb63r9em0c",
+            locality: "Balaji Udhayam Nagar",
+            areaName: "Wadgaon Sheri",
+            costForTwo: "₹300 for two",
+            cuisines: ["Chinese", "Fast Food", "Asian"],
+            avgRating: 3.8,
+            parentId: "13516",
+            avgRatingString: "3.8",
+            totalRatingsString: "4.2K+",
+            sla: {
+                deliveryTime: 36,
+                lastMileTravel: 2.6,
+                serviceability: "SERVICEABLE",
+                slaString: "35-40 mins",
+                lastMileTravelString: "2.6 km",
+                iconType: "ICON_TYPE_EMPTY",
+            },
+            availability: {
+                nextCloseTime: "2024-09-08 06:00:00",
+                opened: true,
+            },
+           
+    },
+    {
+      
+            id: "538756",
+            name: "Fried chicken Destination",
+            cloudinaryImageId: "r6dwrtpkrczdhmyl79yr",
+            locality: "Kharadi",
+            areaName: "Kharadi",
+            costForTwo: "₹200 for two",
+            cuisines: ["Fast Food", "Snacks"],
+            avgRating: 4,
+            parentId: "245841",
+            avgRatingString: "4.0",
+            totalRatingsString: "135",
+            sla: {
+                deliveryTime: 34,
+                lastMileTravel: 3,
+                serviceability: "SERVICEABLE",
+                slaString: "30-35 mins",
+                lastMileTravelString: "3.0 km",
+                iconType: "ICON_TYPE_EMPTY",
+            }
+           
+    },
+    {
+       
+            id: "948129",
+            name: "The Pizza King",
+            cloudinaryImageId:
+                "RX_THUMBNAIL/IMAGES/VENDOR/2024/8/27/dd488d40-f4f2-443f-a9c7-7551989c7f7f_948129.jpg",
+            locality: "Magarpatta",
+            areaName: "Magarpatta",
+            costForTwo: "₹300 for two",
+            cuisines: ["Pizzas", "Cafe"],
+            parentId: "212604",
+            avgRatingString: "NEW",
+            sla: {
+                deliveryTime: 34,
+                lastMileTravel: 3,
+                serviceability: "SERVICEABLE",
+                slaString: "30-35 mins",
+                lastMileTravelString: "3.0 km",
+                iconType: "ICON_TYPE_EMPTY",
+            },
+            availability: {
+                nextCloseTime: "2024-09-07 23:59:00",
+                opened: true,
+            },
+            
+    },
+    {
+        
+            id: "747696",
+            name: "Venky's Chicken Xperience",
+            cloudinaryImageId: "16400d2141cb4bc16ac6b79edfb89298",
+            locality: "Patil Buwa Nagar",
+            areaName: "Kharadi",
+            costForTwo: "₹400 for two",
+            cuisines: [
+                "North Indian",
+                "Burgers",
+                "Biryani",
+                "Tandoor",
+                "Kebabs",
+            ],
+            avgRating: 4.5,
+            parentId: "222688",
+            avgRatingString: "4.5",
+            totalRatingsString: "37",
+            sla: {
+                deliveryTime: 31,
+                lastMileTravel: 2.6,
+                serviceability: "SERVICEABLE",
+                slaString: "30-35 mins",
+                lastMileTravelString: "2.6 km",
+                iconType: "ICON_TYPE_EMPTY",
+            },
+            availability: {
+                nextCloseTime: "2024-09-07 23:00:00",
+                opened: true,
+            },
+         
+    },
+    {
+       
+            id: "636568",
+            name: "Kiosk Kaffee",
+            cloudinaryImageId: "d4680a71533e82c95a66205f92c76cde",
+            locality: "Econ It Park Road",
+            areaName: "Kharadi",
+            costForTwo: "₹200 for two",
+            cuisines: ["Beverages", "Desserts", "Cafe", "Snacks"],
+            avgRating: 4.3,
+            veg: true,
+            parentId: "118722",
+            avgRatingString: "4.3",
+            totalRatingsString: "22",
+            sla: {
+                deliveryTime: 34,
+                lastMileTravel: 3.5,
+                serviceability: "SERVICEABLE",
+                slaString: "30-35 mins",
+                lastMileTravelString: "3.5 km",
+                iconType: "ICON_TYPE_EMPTY",
+            },
+            availability: {
+                nextCloseTime: "2024-09-07 23:30:00",
+                opened: true,
+            },
+          
+    },
+    {
+      
+            id: "801321",
+            name: "Makhani Darbar",
+            cloudinaryImageId: "c583da4b69d264ffe705e5918fad0e98",
+            locality: "Kharadi FC",
+            areaName: "AVDHOOT HIGHTS",
+            costForTwo: "₹500 for two",
+            cuisines: [
+                "Biryani",
+                "North Indian",
+                "Kebabs",
+                "Mughlai",
+                "Beverages",
+                "Desserts",
+            ],
+            avgRating: 4.1,
+            parentId: "478595",
+            avgRatingString: "4.1",
+            totalRatingsString: "16",
+            sla: {
+                deliveryTime: 25,
+                lastMileTravel: 0.8,
+                serviceability: "SERVICEABLE",
+                slaString: "25-30 mins",
+                lastMileTravelString: "0.8 km",
+                iconType: "ICON_TYPE_EMPTY",
+            },
+            availability: {
+                nextCloseTime: "2024-09-07 23:59:00",
+                opened: true,
+            },
+           
+    },
+    {
+        
+            id: "289242",
+            name: "The Dessert Zone",
+            cloudinaryImageId: "c447c9ce7d0bbe78e46141e866236762",
+            locality: "Tukaram Nagar",
+            areaName: "Kharadi",
+            costForTwo: "₹200 for two",
+            cuisines: [
+                "Bakery",
+                "Desserts",
+                "Ice Cream",
+                "Beverages",
+                "Cakes",
+            ],
+            avgRating: 4.1,
+            parentId: "10414",
+            avgRatingString: "4.1",
+            totalRatingsString: "1.7K+",
+            sla: {
+                deliveryTime: 28,
+                lastMileTravel: 1.4,
+                serviceability: "SERVICEABLE",
+                slaString: "25-30 mins",
+                lastMileTravelString: "1.4 km",
+                iconType: "ICON_TYPE_EMPTY",
+            },
+            availability: {
+                nextCloseTime: "2024-09-08 05:00:00",
+                opened: true,
+            },
+           
+            
+    },
+    {
+       
+            id: "576007",
+            name: "Go Zero Ice Creams & Desserts",
+            cloudinaryImageId:
+                "RX_THUMBNAIL/IMAGES/VENDOR/2024/4/23/4c3ce867-d5cb-4820-a6a5-3de83c08b150_576007.JPG",
+            locality: "Thite Wasti",
+            areaName: "Kharadi",
+            costForTwo: "₹150 for two",
+            cuisines: ["Ice Cream", "Desserts", "Healthy Food"],
+            avgRating: 4.3,
+            veg: true,
+            parentId: "343689",
+            avgRatingString: "4.3",
+            totalRatingsString: "48",
+            sla: {
+                deliveryTime: 18,
+                lastMileTravel: 0.8,
+                serviceability: "SERVICEABLE",
+                slaString: "15-20 mins",
+                lastMileTravelString: "0.8 km",
+                iconType: "ICON_TYPE_EMPTY",
+            },
+            availability: {
+                nextCloseTime: "2024-09-07 23:59:00",
+                opened: true,
+            },
+           
+    },
+    {
+        
+            id: "870288",
+            name: "Naadbramha Idli",
+            cloudinaryImageId:
+                "RX_THUMBNAIL/IMAGES/VENDOR/2024/4/22/02edc2a1-b671-4a59-851e-19f6ae024716_870288.JPG",
+            locality: "Bollywood Multiplex",
+            areaName: "Viman Nagar",
+            costForTwo: "₹120 for two",
+            cuisines: ["South Indian"],
+            avgRating: 4.2,
+            veg: true,
+            parentId: "251339",
+            avgRatingString: "4.2",
+            totalRatingsString: "109",
+            sla: {
+                deliveryTime: 19,
+                lastMileTravel: 1.1,
+                serviceability: "SERVICEABLE",
+                slaString: "15-20 mins",
+                lastMileTravelString: "1.1 km",
+                iconType: "ICON_TYPE_EMPTY",
+            },
+            availability: {
+                nextCloseTime: "2024-09-07 21:15:00",
+                opened: true,
+            },
+         
+    },
+    {
+        
+            id: "284022",
+            name: "Monginis",
+            cloudinaryImageId: "sxnfe16bizjxy1hbmjpn",
+            locality: "Kharadi",
+            areaName: "Viman Nagar",
+            costForTwo: "₹400 for two",
+            cuisines: ["Desserts"],
+            avgRating: 4.4,
+            parentId: "5007",
+            avgRatingString: "4.4",
+            totalRatingsString: "331",
+            sla: {
+                deliveryTime: 23,
+                lastMileTravel: 1.4,
+                serviceability: "SERVICEABLE",
+                slaString: "20-25 mins",
+                lastMileTravelString: "1.4 km",
+                iconType: "ICON_TYPE_EMPTY",
+            },
+            availability: {
+                nextCloseTime: "2024-09-07 23:58:00",
+                opened: true,
+            },
+            
+    },
+    {
+      
+            id: "387052",
+            name: "Chitale Bandhu MIthaiwale",
+            cloudinaryImageId: "pccssgmmhlfvmrmpv6wc",
+            locality: "Tukaram Nagar",
+            areaName: "Kharadi",
+            costForTwo: "₹450 for two",
+            cuisines: ["Sweets", "Snacks"],
+            avgRating: 4.5,
+            veg: true,
+            parentId: "10773",
+            avgRatingString: "4.5",
+            totalRatingsString: "2.2K+",
+            sla: {
+                deliveryTime: 16,
+                lastMileTravel: 1.2,
+                serviceability: "SERVICEABLE",
+                slaString: "15-20 mins",
+                lastMileTravelString: "1.2 km",
+                iconType: "ICON_TYPE_EMPTY",
+            },
+            availability: {
+                nextCloseTime: "2024-09-07 22:15:00",
+                opened: true,
+            },
+         
+    },
+    {
+        
+            id: "213853",
+            name: "WarmOven Cake & Desserts",
+            cloudinaryImageId:
+                "RX_THUMBNAIL/IMAGES/VENDOR/2024/5/29/d44b4cae-5218-49a6-8e55-76e44f3b0235_213853.JPG",
+            locality: "Tukaram Nagar",
+            areaName: "Kharadi",
+            costForTwo: "₹200 for two",
+            cuisines: [
+                "Bakery",
+                "Desserts",
+                "Ice Cream",
+                "Beverages",
+                "Cakes",
+            ],
+            avgRating: 4,
+            parentId: "9696",
+            avgRatingString: "4.0",
+            totalRatingsString: "5.2K+",
+            sla: {
+                deliveryTime: 31,
+                lastMileTravel: 1.4,
+                serviceability: "SERVICEABLE",
+                slaString: "30-35 mins",
+                lastMileTravelString: "1.4 km",
+                iconType: "ICON_TYPE_EMPTY",
+            },
+            availability: {
+                nextCloseTime: "2024-09-08 05:00:00",
+                opened: true,
+            },
+          
+    },
+    {
+    
+            id: "572995",
+            name: "NOTO - Ice Creams & Desserts",
+            cloudinaryImageId:
+                "RX_THUMBNAIL/IMAGES/VENDOR/2024/7/22/aac7ab62-305d-4daf-9a93-9d93f0c409e4_572995.JPG",
+            locality: "Matruchaya H Socy Nagar Road",
+            areaName: "Koregaon Park",
+            costForTwo: "₹250 for two",
+            cuisines: ["Ice Cream", "Desserts", "Indian"],
+            avgRating: 4.5,
+            veg: true,
+            parentId: "468478",
+            avgRatingString: "4.5",
+            totalRatingsString: "463",
+            sla: {
+                deliveryTime: 26,
+                lastMileTravel: 2.8,
+                serviceability: "SERVICEABLE",
+                slaString: "25-30 mins",
+                lastMileTravelString: "2.8 km",
+                iconType: "ICON_TYPE_EMPTY",
+            },
+            availability: {
+                nextCloseTime: "2024-09-08 00:00:00",
+                opened: true,
+            },
+          
+    },
+    
+],
